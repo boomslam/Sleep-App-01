001a9d9bb34e44668c40bc475c2af65abfcdbd0b
Jess <jesswlam@gmail.com>
2016-03-17 09:31:21 -0700

    Initial Commit

---
+++ Sleep App 01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sleep App 01
+//
+//  Created by Jess Lam on 3/17/16.
+//  Copyright © 2016 Jess Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sleep App 01Tests/Sleep_App_01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Sleep_App_01Tests.swift
+//  Sleep App 01Tests
+//
+//  Created by Jess Lam on 3/17/16.
+//  Copyright © 2016 Jess Lam. All rights reserved.
+//
+
+import XCTest
+@testable import Sleep_App_01
+
+class Sleep_App_01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
